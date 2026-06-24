c3e8ec830b2d3c8bc301d2ee12e1fd221a7755ca
Nilakshi2006 <Nilakshibhanot@gmail.com>
2026-06-24 21:38:41 +0530

    Day-15

--- Assignments/Assignment-7/Prog2.c
+++ Assignments/Assignment-7/Prog2.c
@@ -5,17 +5,17 @@ int main(){
 
     printf("Enter the number: ");
     scanf("%d",&num);
-    original=num;
+    original=num;//Let number is 121
 
 while(num!=0){
-    rem=num%10;
-    rev=rev*10+rem;
-    num/=10;
+    rem=num%10;  //1->2->1
+    rev=rev*10+rem;//0*10+1=1 -> 1*10+2=12 -> 12*10+1=121
+    num/=10;     //to remove the last digits one by one 
 }
 if(original==rev){
-    printf("%d is Palindrome Number",original);
+    printf("%d is Palindrome Number",original);  //121
 }else{
-    printf("%d is not Palindrome Number",original);
+    printf("%d is not Palindrome Number",original);  //121
 }
 return 0;
 }
